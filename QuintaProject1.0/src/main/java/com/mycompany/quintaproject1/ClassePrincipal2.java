67a395b702981c41e76fcd98731e0c7bb07c4bc5
augusto <augusto@192.168.5.197>
2016-02-02 17:39:28 -0200

    Classe Main criada. Comentários de cabeçalho adicionados.

---
+++ QuintaProject1.0/src/main/java/com/mycompany/quintaproject1/ClassePrincipal2.java
@@ -0,0 +1,25 @@
+/*
+Projeto de conclusão da matéria de Método de Desenvolvimento de Software ministrada 
+pelo professor Marcelo Quinta do Instituto de Informática da Universidade Federal
+de Goiás
+
+Projeto Open-Source criado para auxiliar no aprendizado e evolução de programas 
+feitos na Universidade Federal de Goiás. O Projeto consiste numa search engine 
+para acessar vários tipos de arquivos em vários diretórios através de tags.
+A idéia é usar a API Lucene da Apache Foundation, para construir a search engine.
+
+Alunos: Augusto César da Fonseca Falcão(Desenvolvedor)
+        Igor Queiroz Silva(Desenvolvedor)
+        Guilherme Alves Rosa e Silva(Scrum Master)
+        Jhordan Gabriel Oliveira Magalhães(Desenvolvedor)
+ */
+
+package com.mycompany.quintaproject1; 
+
+/**
+ *
+ * @author Augusto César da Fonseca Falcão
+ */
+public class ClassePrincipal {
+    
+}
